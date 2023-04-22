946e6c93b86d5ad8a334cebda408fdbc93ffd48d
wighawag <wighawag@gmail.com>
2023-04-22 08:10:54 +0100

    AnotherCounter

--- project-to-link/test/AnotherCounter.t.sol
+++ project-to-link/test/AnotherCounter.t.sol
@@ -2,13 +2,13 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/Counter.sol";
+import "../src/AnotherCounter.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+contract AnotherCounterTest is Test {
+    AnotherCounter public counter;
 
     function setUp() public {
-        counter = new Counter();
+        counter = new AnotherCounter();
         counter.setNumber(0);
     }
 
